13d53465bf933f08346238e2bb698538a8a1ef6e
suvorovjr <pglavan1998@mail.ru>
2024-10-04 17:28:36 +0300

    Add get facebook pages APIView in project

--- events/views.py
+++ events/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework import response, status
+from .services import get_facebook_pages
+from django.conf import settings
+from .serializers import FacebookPagesSerializer
+from django.db import transaction
 
-# Create your views here.
+
+class GetFacebookPagesAPIView(views.APIView):
+    @transaction.atomic
+    def post(self, requests, *arg, **kwargs):
+        facebook_pages = get_facebook_pages(access_token=settings.FACEBOOK_ACCESS_TOKEN)
+
+        if facebook_pages is None:
+            return response.Response({'Error': 'Ошибка при отправке запроса.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        created_objects = []
+
+        for page in facebook_pages.data:
+            page_data = {
+                'social_platform_id': page.id,
+                'name': page.name,
+                'page_token': page.access_token
+            }
+            serializer = FacebookPagesSerializer(data=page_data)
+            if serializer.is_valid():
+                created_page = serializer.save()
+                created_objects.append(created_page)
+
+        if len(created_objects) > 1:
+            serializer = FacebookPagesSerializer(created_objects, many=True)
+        else:
+            serializer = FacebookPagesSerializer(created_objects[0])
+
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
